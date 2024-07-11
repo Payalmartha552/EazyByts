b6bc61f7b2150ea1ee51e6c83de2787d362236ed
Payalmartha552 <102712751+Payalmartha552@users.noreply.github.com>
2024-07-11 21:12:01 +0530

    Add files via upload

---
+++ ContactController.java
@@ -0,0 +1,36 @@
+package com.crmsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.crmsystem.entity.Contact;
+import com.crmsystem.service.ContactService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/contacts")
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return contactService.getAllContacts();
+    }
+
+    @PostMapping
+    public Contact createContact(@RequestBody Contact contact) {
+        return contactService.saveContact(contact);
+    }
+
+    @PutMapping("/{id}")
+    public Contact updateContact(@PathVariable Long id, @RequestBody Contact contact) {
+        return contactService.updateContact(id, contact);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteContact(@PathVariable Long id) {
+        contactService.deleteContact(id);
+    }
+}
